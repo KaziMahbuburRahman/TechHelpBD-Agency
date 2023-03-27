c774162b4b5aac8a71be2e4026d6486b58f954e1
NoobMahbub <Mahbubbengali@gmail.com>
2023-03-27 12:37:14 +0600

    Completed services section

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let html = document.getElementById('html');
 let change_btn = document.getElementById('change_btn');
 let change_image = document.getElementById('change_image');
 
-change_btn.addEventListener('click', function(){
+change_btn.addEventListener('click', function () {
     html.classList.toggle('dark');
     change_image.classList.toggle('dark');
+
+    if (change_image.src.includes('img/moon.svg')) {
+        change_image.src = 'img/sun.svg';
+    } else {
+        change_image.src = 'img/moon.svg';
+    }
 });
+
