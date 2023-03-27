ad698d9196fa4a22a4c3b83362dca634a31f721e
NoobMahbub <Mahbubbengali@gmail.com>
2023-03-27 15:58:02 +0600

    fixed the dark mode icon issue

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-//Light mode and dark mode 
-let html = document.getElementById('html');
-let change_btn = document.getElementById('change_btn');
-let change_image = document.getElementById('change_image');
+// Check if there is a theme preference stored in localStorage
+if (localStorage.getItem('theme') === 'dark') {
+    html.classList.remove('dark');
+    change_image.src = 'img/moon.svg';
+} else {
+    html.classList.add('dark');
+    change_image.src = 'img/sun.svg';
 
+}
+
+// Update localStorage with the current theme preference
 change_btn.addEventListener('click', function () {
     html.classList.toggle('dark');
     change_image.classList.toggle('dark');
 
     if (change_image.src.includes('img/moon.svg')) {
         change_image.src = 'img/sun.svg';
+        localStorage.setItem('theme', 'dark');
     } else {
         change_image.src = 'img/moon.svg';
+        localStorage.setItem('theme', 'light');
     }
 });
-
